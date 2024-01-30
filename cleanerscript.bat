38e3d9603c4bb0133b6ec32cd99dd8d547446587
losa.dev <95869100+LewisLosa@users.noreply.github.com>
2024-01-31 01:35:36 +0300

    Update cleanerscript.bat

--- cleanerscript.bat
+++ cleanerscript.bat
@@ -4,46 +4,19 @@ title CLEANERSCRIPT - starting up...
 color c
 echo The color was made red.
 echo After 3 seconds, the process will start...
+@timeout /t 3 /nobreak>nul
 @ECHO OFF
-@timeout /t 5 /nobreak>nul
-echo 1% Completed...
 title CLEANERSCRIPT - cleaning up...
-@timeout /t 1 /nobreak>nul
 @ECHO OFF
 del /f /s /q %windir%\prefetch\*.*
-cls
-echo 28% Completed...
-@timeout /t 1 /nobreak>nul
-@ECHO OFF
 rd /s /q %windir%\temp & md %windir%\temp
-cls
-echo 43% Completed...
-@timeout /t 1 /nobreak>nul
-@ECHO OFF
 del /f /q %userprofile%\cookies\*.*
-cls
-echo 55% Completed...
-@timeout /t 1 /nobreak>nul
-@ECHO OFF
 del /f /q %userprofile%\recent\*.*
-cls
-echo 74% Completed...
-@timeout /t 1 /nobreak>nul
-@ECHO OFF
 del /f /s /q “%userprofile%\Local Settings\Temp\*.*”
-cls
-echo 100% Completed...
-@timeout /t 1 /nobreak>nul
-@ECHO OFF
 del /f /s /q “%userprofile%\recent\*.*”
-cls
-echo wait a second...
-@timeout /t 1 /nobreak>nul
-@ECHO OFF
 rd %temp% /s /q
 md %temp%
 cls
-@ECHO OFF
 title CLEANERSCRIPT - cleaning is complete.
 echo Cleaning is complete.
 echo Cmd will close after 5 seconds.
